b0aec0d1567a511a1383b2bd96dc556714ed8937
lau-ka-ho <asdf913@gmail.com>
2026-09-06 00:26:00 +0800

    Initial

---
+++ src/main/java/com/jcraft/jsch/SshMain.java
@@ -0,0 +1,388 @@
+package com.jcraft.jsch;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.collections4.IteratorUtils;
+import org.apache.commons.configuration2.INIConfiguration;
+import org.apache.commons.configuration2.ImmutableConfiguration;
+import org.apache.commons.configuration2.SubnodeConfiguration;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.function.FailableRunnable;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+public class SshMain {
+
+	public static void main(final String[] args) throws Exception {
+		//
+		Session session = null;
+		//
+		ChannelExec channel = null;
+		//
+		final INIConfiguration iniConfiguration = new INIConfiguration();
+		//
+		final File file = new File("config.ini");
+		//
+		testAndRun(and(file, SshMain::exists, SshMain::isFile, SshMain::canRead), () -> {
+			//
+			try (final Reader reader = new FileReader(file)) {
+				//
+				iniConfiguration.read(reader);
+				//
+			} // try
+				//
+		});
+		//
+		try {
+			//
+			final String user = getString(iniConfiguration, "user");
+			//
+			final String host = getString(iniConfiguration, "host");
+			//
+			final Integer port = testAndApply(NumberUtils::isDigits, getString(iniConfiguration, "port"),
+					Integer::valueOf, null);
+			//
+			setPassword(session = getSession(new JSch(), host, port, user),
+					getBytes(getString(iniConfiguration, "password")));
+			//
+			final Properties config = new Properties();
+			//
+			config.put("StrictHostKeyChecking", "no");
+			//
+			setConfig(session, config);
+			//
+			connect(session);
+			//
+			final SubnodeConfiguration subnodeConfiguration = iniConfiguration.getSection("command");
+			//
+			final List<String> keys = testAndApply(Objects::nonNull, getKeys(subnodeConfiguration),
+					IteratorUtils::toList, null);
+			//
+			sort(keys, (a, b) -> {
+				//
+				if (Boolean.logicalAnd(NumberUtils.isDigits(a), NumberUtils.isDigits(b))) {
+					//
+					return Integer.compare(NumberUtils.toInt(a), NumberUtils.toInt(b));
+					//
+				} // if
+					//
+				return ObjectUtils.compare(a, b);
+				//
+			});
+			//
+			String string = null;
+			//
+			for (int i = 0; i < IterableUtils.size(keys); i++) {
+				//
+				System.out.println(string = getString(subnodeConfiguration, IterableUtils.get(keys, i)));
+				//
+				try (final InputStream is = getInputStream(
+						channel = cast(ChannelExec.class, openChannel(session, "exec")))) {
+					//
+					setCommand(channel, string);
+					//
+					connect(channel);
+					//
+					System.out.println(IOUtils.toString(is, StandardCharsets.UTF_8));
+					//
+					disconnect(channel);
+					//
+				} // if
+					//
+			} // for
+				//
+		} finally {
+			//
+			testAndAccept(SshMain::isConnected, session, SshMain::disconnect);
+			//
+		} // try
+			//
+	}
+
+	private static void disconnect(final Channel instance) throws JSchException {
+		if (instance != null) {
+			instance.disconnect();
+		}
+	}
+
+	private static void connect(final Channel instance) throws JSchException {
+		//
+		if (instance == null) {
+			//
+			return;
+			//
+		} // if
+			//
+		try {
+			//
+			if (Narcissus.getField(instance, Narcissus.findField(getClass(instance), "session")) == null) {
+				//
+				return;
+				//
+			} // if
+				//
+		} catch (final NoSuchFieldException e) {
+			//
+			throw new RuntimeException(e);
+			//
+		} // try
+			//
+		instance.connect();
+		//
+	}
+
+	private static void setCommand(final ChannelExec instance, final String command) {
+		//
+		if (instance == null) {
+			//
+			return;
+			//
+		} // if
+			//
+		try {
+			//
+			if (command != null
+					&& Narcissus.getField(command, Narcissus.findField(getClass(command), "value")) == null) {
+				//
+				return;
+				//
+			} // if
+				//
+		} catch (final NoSuchFieldException e) {
+			//
+			throw new RuntimeException(e);
+			//
+		} // try
+			//
+		instance.setCommand(command);
+		//
+	}
+
+	private static InputStream getInputStream(final Channel instance) throws IOException {
+		//
+		if (instance == null) {
+			//
+			return null;
+			//
+		} // if
+			//
+		try {
+			//
+			if (Narcissus.getField(instance, Narcissus.findField(getClass(instance), "io")) == null) {
+				//
+				return null;
+				//
+			} // if
+				//
+		} catch (final NoSuchFieldException e) {
+			//
+			throw new RuntimeException(e);
+			//
+		} // try
+			//
+		return instance.getInputStream();
+		//
+	}
+
+	private static <T> void testAndAccept(final Predicate<T> predicate, final T value, final Consumer<T> consumer) {
+		if (test(predicate, value)) {
+			accept(consumer, value);
+		}
+	}
+
+	private static <T> void accept(final Consumer<T> instance, final T value) {
+		if (instance != null) {
+			instance.accept(value);
+		}
+	}
+
+	private static <E extends Throwable> void testAndRun(final boolean condition, final FailableRunnable<E> runnable)
+			throws E {
+		if (condition && runnable != null) {
+			runnable.run();
+		}
+	}
+
+	private static <E> void sort(final List<E> instance, final Comparator<? super E> c) {
+		if (instance != null) {
+			instance.sort(c);
+		}
+	}
+
+	private static Channel openChannel(final Session instance, final String type) throws JSchException {
+		return instance != null && instance.isConnected() ? instance.openChannel(type) : null;
+	}
+
+	private static boolean isConnected(final Session instance) {
+		return instance != null && instance.isConnected();
+	}
+
+	private static void disconnect(final Session instance) {
+		if (instance != null) {
+			instance.disconnect();
+		}
+	}
+
+	private static void connect(final Session instance) throws JSchException {
+		//
+		if (instance == null) {
+			//
+			return;
+			//
+		} // if
+			//
+		try {
+			//
+			if (Narcissus.getField(instance, Narcissus.findField(getClass(instance), "jsch")) == null) {
+				//
+				return;
+				//
+			} // if
+				//
+		} catch (final NoSuchFieldException e) {
+			//
+			throw new RuntimeException(e);
+			//
+		} // try
+			//
+		instance.connect();
+		//
+	}
+
+	private static void setConfig(final Session instance, final Properties newconf) {
+		//
+		if (instance == null) {
+			//
+			return;
+			//
+		} // if
+			//
+		try {
+			//
+			if (newconf != null && Narcissus.getField(newconf, Narcissus.findField(getClass(newconf), "map")) == null) {
+				//
+				return;
+				//
+			} // if
+				//
+		} catch (final NoSuchFieldException e) {
+			//
+			throw new RuntimeException(e);
+			//
+		} // try
+			//
+		instance.setConfig(newconf);
+		//
+	}
+
+	private static void setPassword(final Session instnace, final byte[] password) {
+		if (instnace != null) {
+			instnace.setPassword(password);
+		}
+	}
+
+	private static <T> T cast(final Class<T> clz, final Object instance) {
+		return clz != null && clz.isInstance(instance) ? clz.cast(instance) : null;
+	}
+
+	private static String getString(final ImmutableConfiguration instance, final String key) {
+		return instance != null ? instance.getString(key) : null;
+	}
+
+	private static Iterator<String> getKeys(final ImmutableConfiguration instance) {
+		return instance != null ? instance.getKeys() : null;
+	}
+
+	private static boolean canRead(final File instance) {
+		return instance != null && instance.getPath() != null && instance.canRead();
+	}
+
+	private static boolean isFile(final File instance) {
+		return instance != null && instance.getPath() != null && instance.isFile();
+	}
+
+	private static boolean exists(final File instance) {
+		return instance != null && instance.getPath() != null && instance.exists();
+	}
+
+	private static <T> boolean and(final T value, final Predicate<T> a, final Predicate<T> b, final Predicate<T> c) {
+		return test(a, value) && test(b, value) && test(c, value);
+	}
+
+	private static <T> boolean test(final Predicate<T> instance, final T value) {
+		return instance != null && instance.test(value);
+	}
+
+	private static Session getSession(final JSch instance, final String host, final Integer port, final String user)
+			throws JSchException {
+		//
+		if (instance == null) {
+			//
+			return null;
+			//
+		} else if (port != null) {
+			//
+			return instance.getSession(user, host, port.intValue());
+			//
+		} // if
+			//
+		return host != null ? instance.getSession(user, host) : null;
+		//
+	}
+
+	private static byte[] getBytes(final String instance) {
+		//
+		if (instance == null) {
+			//
+			return null;
+			//
+		} // if
+			//
+		try {
+			//
+			if (Narcissus.getField(instance, Narcissus.findField(getClass(instance), "value")) == null) {
+				//
+				return null;
+				//
+			} // if
+				//
+		} catch (final NoSuchFieldException e) {
+			//
+			throw new RuntimeException(e);
+			//
+		} // try
+			//
+		return instance.getBytes();
+		//
+	}
+
+	private static Class<?> getClass(final Object instance) {
+		return instance != null ? instance.getClass() : null;
+	}
+
+	private static <T, R> R testAndApply(final Predicate<T> predicate, final T value, final Function<T, R> functionTrue,
+			final Function<T, R> functionFalse) {
+		return test(predicate, value) ? apply(functionTrue, value) : apply(functionFalse, value);
+	}
+
+	private static <T, R> R apply(final Function<T, R> instance, final T value) {
+		return instance != null ? instance.apply(value) : null;
+	}
+
+}
---
+++ src/test/java/com/jcraft/jsch/SshMainTest.java
@@ -0,0 +1,352 @@
+package com.jcraft.jsch;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.apache.commons.configuration2.ImmutableConfiguration;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.google.common.base.Predicates;
+import com.google.common.reflect.Reflection;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+public class SshMainTest {
+
+	private static Method METHOD_EXISTS, METHOD_IS_FILE, METHOD_CAN_READ, METHOD_AND, METHOD_CAST = null;
+
+	@BeforeSuite
+	void beforeSuite() throws NoSuchMethodException {
+		//
+		final Class<?> clz = SshMain.class;
+		//
+		(METHOD_EXISTS = clz.getDeclaredMethod("exists", File.class)).setAccessible(true);
+		//
+		(METHOD_IS_FILE = clz.getDeclaredMethod("isFile", File.class)).setAccessible(true);
+		//
+		(METHOD_CAN_READ = clz.getDeclaredMethod("canRead", File.class)).setAccessible(true);
+		//
+		(METHOD_AND = clz.getDeclaredMethod("and", Object.class, Predicate.class, Predicate.class, Predicate.class))
+				.setAccessible(true);
+		//
+		(METHOD_CAST = clz.getDeclaredMethod("cast", Class.class, Object.class)).setAccessible(true);
+		//
+	}
+
+	private static class IH implements InvocationHandler {
+
+		private Boolean test;
+
+		@Override
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			final String name = getName(method);
+			//
+			if (Objects.equals(getReturnType(method), Void.TYPE)) {
+				//
+				return null;
+				//
+			} // if
+				//
+			if (proxy instanceof Function && Objects.equals(name, "apply")) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof Predicate && Objects.equals(name, "test")) {
+				//
+				return test;
+				//
+			} else if (proxy instanceof ImmutableConfiguration
+					&& contains(Arrays.asList("getKeys", "getString"), name)) {
+				//
+				return null;
+				//
+			} // if
+				//
+			throw new Throwable(name);
+			//
+		}
+
+		private static boolean contains(final Collection<?> instance, final Object item) {
+			return instance != null && instance.contains(item);
+		}
+
+	}
+
+	private static Class<?> getReturnType(final Method instance) {
+		return instance != null ? instance.getReturnType() : null;
+	}
+
+	private File file = null;
+
+	@BeforeMethod
+	void beforeMethod() {
+		//
+		file = new File("pom.xml");
+		//
+	}
+
+	@Test
+	void testNull() throws Throwable {
+		//
+		final Method[] ms = SshMain.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		Class<?>[] parameterTypes = null;
+		//
+		Object result = null;
+		//
+		String toString = null;
+		//
+		Collection<Object> collection = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ArrayUtils.get(ms, i)) == null || m.isSynthetic()
+					|| (parameterTypes = m.getParameterTypes()) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			clear(collection = ObjectUtils.getIfNull(collection, ArrayList::new));
+			//
+			for (int j = 0; j < parameterTypes.length; j++) {
+				//
+				if (Objects.equals(ArrayUtils.get(parameterTypes, j), Boolean.TYPE)) {
+					//
+					add(collection, Boolean.TRUE);
+					//
+				} else {
+					//
+					add(collection, null);
+					//
+				} // if
+					//
+			} // for
+
+			result = Narcissus.invokeStaticMethod(m, toArray(collection));
+			//
+			toString = Objects.toString(m);
+			//
+			if (Objects.equals(getReturnType(m), Boolean.TYPE)) {
+				//
+				Assert.assertNotNull(result, toString);
+				//
+			} else {
+				//
+				Assert.assertNull(result, toString);
+				//
+			} // if
+				//
+		} // for
+			//
+	}
+
+	private static Object[] toArray(final Collection<?> instance) {
+		return instance != null ? instance.toArray() : null;
+	}
+
+	@Test
+	void testNotNull() throws Throwable {
+		//
+		final Method[] ms = SshMain.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		Class<?>[] parameterTypes = null;
+		//
+		Class<?> parameterType = null;
+		//
+		Object result = null;
+		//
+		String toString, name = null;
+		//
+		Collection<Object> collection = null;
+		//
+		IH ih = null;
+		//
+		Field[] fs = null;
+		//
+		Field f = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ArrayUtils.get(ms, i)) == null || m.isSynthetic()
+					|| (parameterTypes = m.getParameterTypes()) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			clear(collection = ObjectUtils.getIfNull(collection, ArrayList::new));
+			//
+			for (int j = 0; j < parameterTypes.length; j++) {
+				//
+				if ((parameterType = ArrayUtils.get(parameterTypes, j)) != null && parameterType.isArray()) {
+					//
+					add(collection, Array.newInstance(parameterType.getComponentType(), 0));
+					//
+				} else if (Objects.equals(parameterType, Boolean.TYPE)) {
+					//
+					add(collection, Boolean.TRUE);
+					//
+				} else if (Objects.equals(parameterType, Class.class)) {
+					//
+					add(collection, Class.class);
+					//
+				} else if (Objects.equals(parameterType, Channel.class)) {
+					//
+					add(collection, Narcissus.allocateInstance(ChannelExec.class));
+					//
+				} else if (parameterType != null && parameterType.isInterface()) {
+					//
+					if ((ih = ObjectUtils.getIfNull(ih, IH::new)) != null
+							&& (fs = IH.class.getDeclaredFields()) != null) {
+						//
+						for (int k = 0; k < fs.length; k++) {
+							//
+							if ((f = ArrayUtils.get(fs, k)) == null) {
+								//
+								continue;
+								//
+							} // if
+								//
+							if (Objects.equals(f.getType(), Boolean.class)) {
+								//
+								Narcissus.setField(ih, f, Boolean.TRUE);
+								//
+							} // if
+								//
+						} // for
+							//
+					} // if
+						//
+					add(collection, Reflection.newProxy(parameterType, ih = ObjectUtils.getIfNull(ih, IH::new)));
+					//
+				} else {
+					//
+					add(collection, Narcissus.allocateInstance(parameterType));
+					//
+				} // if
+					//
+			} // for
+				//
+			result = Narcissus.invokeStaticMethod(m, toArray(collection));
+			//
+			toString = Objects.toString(m);
+			//
+			if (Objects.equals(getReturnType(m), Void.TYPE)
+					|| Boolean.logicalAnd(Objects.equals(name = getName(m), "getBytes"),
+							Arrays.equals(parameterTypes, new Class<?>[] { String.class }))
+					|| Boolean.logicalAnd(Objects.equals(name, "apply"),
+							Arrays.equals(parameterTypes, new Class<?>[] { Function.class, Object.class }))
+					|| Boolean.logicalAnd(Objects.equals(name, "testAndApply"),
+							Arrays.equals(parameterTypes,
+									new Class<?>[] { Predicate.class, Object.class, Function.class, Function.class }))
+					|| Boolean.logicalAnd(Objects.equals(name, "getKeys"),
+							Arrays.equals(parameterTypes, new Class<?>[] { ImmutableConfiguration.class }))
+					|| Boolean.logicalAnd(Objects.equals(name, "getString"),
+							Arrays.equals(parameterTypes,
+									new Class<?>[] { ImmutableConfiguration.class, String.class }))
+					|| Boolean.logicalAnd(Objects.equals(name, "cast"),
+							Arrays.equals(parameterTypes, new Class<?>[] { Class.class, Object.class }))
+					|| Boolean.logicalAnd(Objects.equals(name, "openChannel"),
+							Arrays.equals(parameterTypes, new Class<?>[] { Session.class, String.class }))
+					|| Boolean.logicalAnd(Objects.equals(name, "getInputStream"),
+							Arrays.equals(parameterTypes, new Class<?>[] { Channel.class }))) {
+				//
+				Assert.assertNull(result, toString);
+				//
+			} else {
+				//
+				Assert.assertNotNull(result, toString);
+				//
+			} // if
+				//
+		} // for
+			//
+	}
+
+	private static String getName(final Member instance) {
+		return instance != null ? instance.getName() : null;
+	}
+
+	private static <E> void add(final Collection<E> instance, final E item) {
+		if (instance != null) {
+			instance.add(item);
+		}
+	}
+
+	private static void clear(final Collection<?> instance) {
+		if (instance != null) {
+			instance.clear();
+		}
+	}
+
+	@Test
+	public void testExists() throws IllegalAccessException, InvocationTargetException {
+		//
+		Assert.assertEquals(invoke(METHOD_EXISTS, null, file), Boolean.TRUE);
+		//
+	}
+
+	@Test
+	public void testIsFile() throws IllegalAccessException, InvocationTargetException {
+		//
+		Assert.assertEquals(invoke(METHOD_IS_FILE, null, file), Boolean.TRUE);
+		//
+		Assert.assertEquals(invoke(METHOD_IS_FILE, null, new File(".")), Boolean.FALSE);
+		//
+	}
+
+	@Test
+	public void testCanRead() throws IllegalAccessException, InvocationTargetException {
+		//
+		Assert.assertEquals(invoke(METHOD_CAN_READ, null, file), Boolean.TRUE);
+		//
+	}
+
+	private static Object invoke(final Method method, final Object instance, final Object... args)
+			throws IllegalAccessException, InvocationTargetException {
+		return method != null && method.getDeclaringClass() != null ? method.invoke(instance, args) : null;
+	}
+
+	@Test
+	public void testAnd() throws IllegalAccessException, InvocationTargetException {
+		//
+		final Predicate<?> alwaysTrue = Predicates.alwaysTrue();
+		//
+		Assert.assertEquals(invoke(METHOD_AND, null, null, alwaysTrue, null, null), Boolean.FALSE);
+		//
+		Assert.assertEquals(invoke(METHOD_AND, null, null, alwaysTrue, alwaysTrue, null), Boolean.FALSE);
+		//
+	}
+
+	@Test
+	public void testCast() throws IllegalAccessException, InvocationTargetException {
+		//
+		final Object object = new Object();
+		//
+		Assert.assertSame(invoke(METHOD_CAST, null, Object.class, object), object);
+		//
+	}
+
+}
